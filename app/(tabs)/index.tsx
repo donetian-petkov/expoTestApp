3ed97668eae0c70cd12c1a27ceb35e1a46cc29e9
donetian-petkov <73138116+donetian-petkov@users.noreply.github.com>
2025-03-16 21:16:24 +0200

    feat: Image Picker Functionality feat: Image Viewer Component feat: Button Component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,43 @@
-import { Text, View, StyleSheet } from "react-native";
-import { Link } from 'expo-router';
+import {View, StyleSheet} from 'react-native';
+import {Image} from 'expo-image';
+import ImageViewer from "@/components/ImageViewer";
+import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
+
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-        <Text style={styles.text}>Home screen</Text>
-        <Link href="/about" style={styles.button}>
-            Go to About screen
-        </Link>
-    </View>
-  );
+
+    const [ selectedImage, setSelectedImage ] = useState<string | undefined>(undefined)
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'],
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!result.canceled) {
+            setSelectedImage(result.assets[0].uri)
+        } else {
+            alert('You did not select any image.');
+        }
+    };
+
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.imageContainer}>
+                <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage}/>
+            </View>
+            <View style={styles.footerContainer}>
+                <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+                <Button label="Use this photo" />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -23,15 +45,17 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#25292e',
         alignItems: 'center',
-        justifyContent: 'center',
     },
-    text: {
-        color: '#000',
+    imageContainer: {
+        flex: 1,
     },
-    button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: '#000',
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
     },
-
-})
+    footerContainer: {
+        flex: 1 / 3,
+        alignItems: 'center',
+    },
+});
